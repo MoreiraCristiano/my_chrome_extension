4c31c6f4e37904896e7b16421d0848224e2a9b13
Cristiano A <moreiracristiano702@gmail.com>
2022-09-03 23:11:35 -0300

    first commit

---
+++ google/index.js
@@ -0,0 +1,8 @@
+const header = document.getElementById("gb");
+header.classList.add("hidden");
+
+const text = document.getElementById("SIvCob");
+text.innerHTML = "";
+
+const footer = document.getElementsByClassName("o3j99");
+footer[4].classList.add("hidden");
---
+++ home/index.js
@@ -0,0 +1,13 @@
+let clock_div = document.getElementById("clock");
+let date_div = document.getElementById("date");
+
+const week = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"];
+
+setInterval(() => {
+  const fullDate = new Date();
+  const time = fullDate.toLocaleTimeString();
+  const date = fullDate.toLocaleDateString();
+  let day = week[fullDate.getDay()];
+  clock_div.innerText = time;
+  date_div.innerText = `${date} - ${day}`;
+}, 1000);
