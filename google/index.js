c8d6baaacba6b0259ebd9dd4cca41cd9d16f6308
Cristiano A <moreiracristiano702@gmail.com>
2022-09-04 21:24:57 -0300

    name

--- google/index.js
+++ google/index.js
@@ -1,8 +1,12 @@
 const header = document.getElementById("gb");
 header.classList.add("hidden");
 
-const text = document.getElementById("SIvCob");
-text.innerHTML = "";
-
 const footer = document.getElementsByClassName("o3j99");
 footer[4].classList.add("hidden");
+
+const text = document.getElementById("SIvCob");
+if (text.innerText == undefined) {
+  throw new Error("Af");
+} else {
+  text.innerHTML = "";
+}
